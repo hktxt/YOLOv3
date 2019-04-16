0819650e4b79a6379dc03cfbec901abe4ca86754
Max <hktxt2011@gmail.com>
2019-04-16 22:53:41 +0800

    4/16
    4/16

---
+++ utils/utils.py
@@ -0,0 +1,8 @@
+def xyxy2xywh(x):
+    # Convert bounding box format from [x1, y1, x2, y2] to [x, y, w, h]
+    y = torch.zeros_like(x) if isinstance(x, torch.Tensor) else np.zeros_like(x)
+    y[:, 0] = (x[:, 0] + x[:, 2]) / 2
+    y[:, 1] = (x[:, 1] + x[:, 3]) / 2
+    y[:, 2] = x[:, 2] - x[:, 0]
+    y[:, 3] = x[:, 3] - x[:, 1]
+    return y
