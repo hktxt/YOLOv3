ea5fd343af9666b6b9b1973b330afabf2cb57ce3
Max <hktxt2011@gmailcom>
2019-04-17 16:58:32 +0800

    4/17

---
+++ test.py
@@ -0,0 +1,134 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import numpy as np
+from models import *
+from utils.datasets import *
+from utils.utils import *
+
+def test(
+        cfg,
+        data_cfg,
+        weights=None,
+        batch_size=16,
+        img_size=416,
+        iou_thres=0.5,
+        conf_thres=0.001,
+        nms_thres=0.5,
+        model=None
+):
+    if model is None:
+        device = torch_utils.select_device()
+
+        # Initialize model
+        model = Darknet(cfg, img_size).to(device)
+
+        # Load weights
+        if weights.endswith('.pt'):  # pytorch format
+            model.load_state_dict(torch.load(weights, map_location=device)['model'])
+        else:  # darknet format
+            _ = load_darknet_weights(model, weights)
+
+        if torch.cuda.device_count() > 1:
+            model = nn.DataParallel(model)
+    else:
+        device = next(model.parameters()).device  # get model device
+
+    # Configure run
+    data_cfg = parse_data_cfg(data_cfg)
+    nc = int(data_cfg['classes'])  # number of classes
+    test_path = data_cfg['valid']  # path to test images
+    names = load_classes(data_cfg['names'])  # class names
+
+    # Dataloader
+    dataset = LoadDataset(test_path, img_size=img_size)
+    dataloader = DataLoader(dataset,
+                            batch_size=batch_size,
+                            num_workers=4,
+                            pin_memory=True,
+                            collate_fn=dataset.collate_fn)
+
+    seen = 0
+    model.eval()
+    print(('%20s' + '%10s' * 6) % ('Class', 'Images', 'Targets', 'P', 'R', 'mAP', 'F1'))
+    loss, p, r, f1, mp, mr, map, mf1 = 0., 0., 0., 0., 0., 0., 0., 0.
+    jdict, stats, ap, ap_class = [], [], [], []
+    for batch_i, (imgs, targets, paths, shapes) in enumerate(tqdm(dataloader, desc='Computing mAP')):
+        targets = targets.to(device)
+        imgs = imgs.to(device)
+
+        # Plot images with bounding boxes
+        if batch_i == 0 and not os.path.exists('test_batch0.jpg'):
+            plot_images(imgs=imgs, targets=targets, fname='test_batch0.jpg')
+
+        # Run model
+        inf_out, train_out = model(imgs)  # inference and training outputs
+
+        # Build targets
+        target_list = build_targets(model, targets)
+
+        # Compute loss
+        loss_i, _ = compute_loss(train_out, target_list)
+        loss += loss_i.item()
+
+        # Run NMS
+        output = non_max_suppression(inf_out, conf_thres=conf_thres, nms_thres=nms_thres)
+
+        # Statistics per image
+        for si, pred in enumerate(output):
+            labels = targets[targets[:, 0] == si, 1:]
+            nl = len(labels)
+            tcls = labels[:, 0].tolist() if nl else []  # target class
+            seen += 1
+
+            if pred is None:
+                if nl:
+                    stats.append(([], torch.Tensor(), torch.Tensor(), tcls))
+                continue
+
+            # Assign all predictions as incorrect
+            correct = [0] * len(pred)
+            if nl:
+                detected = []
+                tbox = xywh2xyxy(labels[:, 1:5]) * img_size  # target boxes
+
+                # Search for correct predictions
+                for i, (*pbox, pconf, pcls_conf, pcls) in enumerate(pred):
+
+                    # Break if all targets already located in image
+                    if len(detected) == nl:
+                        break
+
+                    # Continue if predicted class not among image classes
+                    if pcls.item() not in tcls:
+                        continue
+
+                    # Best iou, index between pred and targets
+                    iou, bi = bbox_iou(pbox, tbox).max(0)
+
+                    # If iou > threshold and class is correct mark as correct
+                    if iou > iou_thres and bi not in detected:
+                        correct[i] = 1
+                        detected.append(bi)
+
+            # Append statistics (correct, conf, pcls, tcls)
+            stats.append((correct, pred[:, 4].cpu(), pred[:, 6].cpu(), tcls))
+
+    # Compute statistics
+    stats_np = [np.concatenate(x, 0) for x in list(zip(*stats))]
+    nt = np.bincount(stats_np[3].astype(np.int64), minlength=nc)  # number of targets per class
+    if len(stats_np):
+        p, r, ap, f1, ap_class = ap_per_class(*stats_np)
+        mp, mr, map, mf1 = p.mean(), r.mean(), ap.mean(), f1.mean()
+
+    # Print results
+    pf = '%20s' + '%10.3g' * 6  # print format
+    print(pf % ('all', seen, nt.sum(), mp, mr, map, mf1), end='\n\n')
+
+    # Print results per class
+    if nc > 1 and len(stats_np):
+        for i, c in enumerate(ap_class):
+            print(pf % (names[c], seen, nt[c], p[i], r[i], ap[i], f1[i]))
+
+    # Return results
+    return mp, mr, map, mf1, loss / len(dataloader)
--- utils/utils.py
+++ utils/utils.py
@@ -141,3 +141,171 @@ def compute_loss(p, targets):  # predictions, targets
     loss = lxy + lwh + lconf + lcls
 
     return loss, torch.cat((lxy, lwh, lconf, lcls, loss)).detach()
+
+def load_classes(path):
+    # Loads class labels at 'path'
+    with open(path, 'r') as fp:
+        names = fp.read().split('\n')
+    return list(filter(None, names))  # filter removes empty strings (such as last line)
+
+def non_max_suppression(prediction, conf_thres=0.5, nms_thres=0.5):
+    """
+    Removes detections with lower object confidence score than 'conf_thres'
+    Non-Maximum Suppression to further filter detections.
+    Returns detections with shape:
+        (x1, y1, x2, y2, object_conf, class_conf, class)
+    """
+
+    min_wh = 2  # (pixels) minimum box width and height
+
+    output = [None] * len(prediction)
+    for image_i, pred in enumerate(prediction):
+        # Experiment: Prior class size rejection
+        # x, y, w, h = pred[:, 0], pred[:, 1], pred[:, 2], pred[:, 3]
+        # a = w * h  # area
+        # ar = w / (h + 1e-16)  # aspect ratio
+        # n = len(w)
+        # log_w, log_h, log_a, log_ar = torch.log(w), torch.log(h), torch.log(a), torch.log(ar)
+        # shape_likelihood = np.zeros((n, 60), dtype=np.float32)
+        # x = np.concatenate((log_w.reshape(-1, 1), log_h.reshape(-1, 1)), 1)
+        # from scipy.stats import multivariate_normal
+        # for c in range(60):
+        # shape_likelihood[:, c] =
+        #   multivariate_normal.pdf(x, mean=mat['class_mu'][c, :2], cov=mat['class_cov'][c, :2, :2])
+
+        # Multiply conf by class conf to get combined confidence
+        class_conf, class_pred = pred[:, 5:].max(1)
+        pred[:, 4] *= class_conf
+
+        # Select only suitable predictions
+        i = (pred[:, 4] > conf_thres) & (pred[:, 2:4] > min_wh).all(1) & (torch.isnan(pred).any(1) == 0)
+        pred = pred[i]
+
+        # If none are remaining => process next image
+        if len(pred) == 0:
+            continue
+
+        # Select predicted classes
+        class_conf = class_conf[i]
+        class_pred = class_pred[i].unsqueeze(1).float()
+
+        # Box (center x, center y, width, height) to (x1, y1, x2, y2)
+        pred[:, :4] = xywh2xyxy(pred[:, :4])
+        # pred[:, 4] *= class_conf  # improves mAP from 0.549 to 0.551
+
+        # Detections ordered as (x1y1x2y2, obj_conf, class_conf, class_pred)
+        pred = torch.cat((pred[:, :5], class_conf.unsqueeze(1), class_pred), 1)
+
+        # Get detections sorted by decreasing confidence scores
+        pred = pred[(-pred[:, 4]).argsort()]
+
+        det_max = []
+        nms_style = 'MERGE'  # 'OR' (default), 'AND', 'MERGE' (experimental)
+        for c in pred[:, -1].unique():
+            dc = pred[pred[:, -1] == c]  # select class c
+            dc = dc[:min(len(dc), 100)]  # limit to first 100 boxes: https://github.com/ultralytics/yolov3/issues/117
+
+            # No NMS required if only 1 prediction
+            if len(dc) == 1:
+                det_max.append(dc)
+                continue
+
+            # Non-maximum suppression
+            if nms_style == 'OR':  # default
+                # METHOD1
+                # ind = list(range(len(dc)))
+                # while len(ind):
+                # j = ind[0]
+                # det_max.append(dc[j:j + 1])  # save highest conf detection
+                # reject = (bbox_iou(dc[j], dc[ind]) > nms_thres).nonzero()
+                # [ind.pop(i) for i in reversed(reject)]
+
+                # METHOD2
+                while dc.shape[0]:
+                    det_max.append(dc[:1])  # save highest conf detection
+                    if len(dc) == 1:  # Stop if we're at the last detection
+                        break
+                    iou = bbox_iou(dc[0], dc[1:])  # iou with other boxes
+                    dc = dc[1:][iou < nms_thres]  # remove ious > threshold
+
+            elif nms_style == 'AND':  # requires overlap, single boxes erased
+                while len(dc) > 1:
+                    iou = bbox_iou(dc[0], dc[1:])  # iou with other boxes
+                    if iou.max() > 0.5:
+                        det_max.append(dc[:1])
+                    dc = dc[1:][iou < nms_thres]  # remove ious > threshold
+
+            elif nms_style == 'MERGE':  # weighted mixture box
+                while len(dc):
+                    if len(dc) == 1:
+                        det_max.append(dc)
+                        break
+                    i = bbox_iou(dc[0], dc) > nms_thres  # iou with other boxes
+                    weights = dc[i, 4:5]
+                    dc[0, :4] = (weights * dc[i, :4]).sum(0) / weights.sum()
+                    det_max.append(dc[:1])
+                    dc = dc[i == 0]
+
+        if len(det_max):
+            det_max = torch.cat(det_max)  # concatenate
+            output[image_i] = det_max[(-det_max[:, 4]).argsort()]  # sort
+
+    return output
+
+
+def ap_per_class(tp, conf, pred_cls, target_cls):
+    """ Compute the average precision, given the recall and precision curves.
+    Source: https://github.com/rafaelpadilla/Object-Detection-Metrics.
+    # Arguments
+        tp:    True positives (list).
+        conf:  Objectness value from 0-1 (list).
+        pred_cls: Predicted object classes (list).
+        target_cls: True object classes (list).
+    # Returns
+        The average precision as computed in py-faster-rcnn.
+    """
+
+    # Sort by objectness
+    i = np.argsort(-conf)
+    tp, conf, pred_cls = tp[i], conf[i], pred_cls[i]
+
+    # Find unique classes
+    unique_classes = np.unique(target_cls)
+
+    # Create Precision-Recall curve and compute AP for each class
+    ap, p, r = [], [], []
+    for c in unique_classes:
+        i = pred_cls == c
+        n_gt = (target_cls == c).sum()  # Number of ground truth objects
+        n_p = i.sum()  # Number of predicted objects
+
+        if n_p == 0 and n_gt == 0:
+            continue
+        elif n_p == 0 or n_gt == 0:
+            ap.append(0)
+            r.append(0)
+            p.append(0)
+        else:
+            # Accumulate FPs and TPs
+            fpc = (1 - tp[i]).cumsum()
+            tpc = (tp[i]).cumsum()
+
+            # Recall
+            recall_curve = tpc / (n_gt + 1e-16)
+            r.append(recall_curve[-1])
+
+            # Precision
+            precision_curve = tpc / (tpc + fpc)
+            p.append(precision_curve[-1])
+
+            # AP from recall-precision curve
+            ap.append(compute_ap(recall_curve, precision_curve))
+
+            # Plot
+            # plt.plot(recall_curve, precision_curve)
+
+    # Compute F1 score (harmonic mean of precision and recall)
+    p, r, ap = np.array(p), np.array(r), np.array(ap)
+    f1 = 2 * p * r / (p + r + 1e-16)
+
+    return p, r, ap, f1, unique_classes.astype('int32')
