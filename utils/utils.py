bd28a2432b29463d24bd3f11e3bc76e78579f6f0
Max <hktxt2011@gmailcom>
2019-04-18 11:42:13 +0800

    4/18

--- utils/utils.py
+++ utils/utils.py
@@ -372,3 +372,30 @@ def plot_results(start=0, stop=0):  # from utils.utils import *; plot_results()
                 plt.legend()
     fig.tight_layout()
     fig.savefig('results.png', dpi=300)
+    
+def compute_ap(recall, precision):
+    """ Compute the average precision, given the recall and precision curves.
+    Source: https://github.com/rbgirshick/py-faster-rcnn.
+    # Arguments
+        recall:    The recall curve (list).
+        precision: The precision curve (list).
+    # Returns
+        The average precision as computed in py-faster-rcnn.
+    """
+    # correct AP calculation
+    # first append sentinel values at the end
+
+    mrec = np.concatenate(([0.], recall, [1.]))
+    mpre = np.concatenate(([0.], precision, [0.]))
+
+    # compute the precision envelope
+    for i in range(mpre.size - 1, 0, -1):
+        mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+
+    # to calculate area under PR curve, look for points
+    # where X axis (recall) changes value
+    i = np.where(mrec[1:] != mrec[:-1])[0]
+
+    # and sum (\Delta recall) * prec
+    ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+    return ap
