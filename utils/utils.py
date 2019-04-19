c55cf13bce4c291924e82ca1c0a0f92a2b400a0e
Max <hktxt2011@gmailcom>
2019-04-19 11:20:45 +0800

    4/19

--- utils/utils.py
+++ utils/utils.py
@@ -68,6 +68,16 @@ def plot_images(imgs, targets, fname='images.jpg'):
     
 def build_targets(model, targets):
     # targets = [image, class, x, y, w, h]
+    """
+    tensor([[ 0.00000, 45.00000,  0.49307,  0.60885,  0.91218,  0.42523],
+        [ 0.00000, 45.00000,  0.73586,  0.29364,  0.47691,  0.34016],
+        [ 0.00000, 50.00000,  0.64325,  0.64040,  0.47253,  0.36455],
+        [ 0.00000, 45.00000,  0.35859,  0.41617,  0.64955,  0.55797],
+        [ 0.00000, 49.00000,  0.65001,  0.21179,  0.11278,  0.06922],
+        [ 0.00000, 49.00000,  0.77024,  0.20984,  0.08678,  0.06942],
+        [ 0.00000, 49.00000,  0.67084,  0.27915,  0.12559,  0.10488],
+        [ 0.00000, 49.00000,  0.64599,  0.17371,  0.14157,  0.10572]])
+    """
     if type(model) in (nn.parallel.DataParallel, nn.parallel.DistributedDataParallel):
         model = model.module
 
@@ -79,58 +89,108 @@ def build_targets(model, targets):
         # iou of targets-anchors
         t, a = targets, []
         gwh = targets[:, 4:6] * layer.nG
+        """
+        tensor([[11.85836,  5.52798],
+        [ 6.19979,  4.42211],
+        [ 6.14289,  4.73918],
+        [ 8.44410,  7.25367],
+        [ 1.46616,  0.89980],
+        [ 1.12818,  0.90246],
+        [ 1.63269,  1.36346],
+        [ 1.84043,  1.37430]])
+        """
         if nt:
-            iou = [wh_iou(x, gwh) for x in layer.anchor_vec] # shape:3*objects
-            iou, a = torch.stack(iou, 0).max(0)  # best iou and anchor, shape:1*objects
+            iou = [wh_iou(x, gwh) for x in layer.anchor_vec] # shape:3*objects(8)
+            iou, a = torch.stack(iou, 0).max(0)  # best iou and anchor, shape:1*objects(8)
             # iou tensor([0.52454, 0.60101, 0.63675, 0.57156, 0.20339, 0.15697, 0.34320, 0.38994], device='cuda:0')
             # a tensor([2, 1, 1, 2, 0, 0, 0, 0], device='cuda:0')
 
             # reject below threshold ious (OPTIONAL, increases P, lowers R)
-            reject = True
+            reject = True # 滤除iou<=0.10的target,防止偏移过大导致loss不稳定，只计算iou大于阈值的loss
             if reject:
                 j = iou > 0.10
                 t, a, gwh = targets[j], a[j], gwh[j]
 
         # Indices
-        b, c = t[:, :2].long().t()  # target image, class
-        gxy = t[:, 2:4] * layer.nG
-        gi, gj = gxy.long().t()  # grid_i, grid_j
-        indices.append((b, a, gj, gi))
+        b, c = t[:, :2].long().t()  # target image, class, b tensor([0, 0, 0, 0, 0, 0, 0, 0]), c tensor([45, 45, 50, 45, 49, 49, 49, 49])
+        gxy = t[:, 2:4] * layer.nG # torch.Size([8, 2])
+        """
+        tensor([[ 6.40985,  7.91507],
+        [ 9.56623,  3.81726],
+        [ 8.36229,  8.32523],
+        [ 4.66172,  5.41020],
+        [ 8.45008,  2.75321],
+        [10.01309,  2.72789],
+        [ 8.72088,  3.62892],
+        [ 8.39792,  2.25823]])
+        """
+        gi, gj = gxy.long().t()  # grid_i, grid_j, 取整，得到坐标所在的网格， 6.8->6, 9.1->9, tensor([ 6,  9,  8,  4,  8, 10,  8,  8]), tensor([7, 3, 8, 5, 2, 2, 3, 2])
+        indices.append((b, a, gj, gi)) # index, anchor, grid j, grid i
 
         # XY coordinates
         txy.append(gxy - gxy.floor())
+        """ 
+        <class 'list'>: 
+        [tensor([[0.40985, 0.91507],
+        [0.56623, 0.81726],
+        [0.36229, 0.32523],
+        [0.66172, 0.41020],
+        [0.45008, 0.75321],
+        [0.01309, 0.72789],
+        [0.72088, 0.62892],
+        [0.39792, 0.25823]])]"""
 
         # Width and height
         twh.append(torch.log(gwh / layer.anchor_vec[a]))  # wh yolo method
+        """
+        <class 'list'>: [tensor([[ 0.09813, -0.58963],
+        [ 0.38498, -0.17755],
+        [ 0.37576, -0.10830],
+        [-0.24144, -0.31795],
+        [-0.54607, -1.04657],
+        [-0.80810, -1.04361],
+        [-0.43849, -0.63096],
+        [-0.31872, -0.62304]])]
+        """
         # twh.append((gwh / layer.anchor_vec[a]) ** (1 / 3) / 2)  # wh power method
 
         # Class
-        tcls.append(c)
+        tcls.append(c) # <class 'list'>: [tensor([45, 45, 50, 45, 49, 49, 49, 49])]
         if c.shape[0]:
             assert c.max() <= layer.nC, 'Target classes exceed model classes'
 
     return txy, twh, tcls, indices
+    # txy: list of len 2, torch.Size([8, 2]), torch.Size([4, 2]) 维度不一样是reject掉了iou<=0.10
+    # twh: same as txy
+    # tcls: list of len 2, tensor([45, 45, 50, 45, 49, 49, 49, 49]), tensor([49, 49, 49, 49])
+    # indices: list of len 2, 4*8, 4*4
 
 
 def compute_loss(p, targets):  # predictions, targets
+    # p: list of len 2, torch.Size([1, 3, 13, 13, 85]), torch.Size([1, 3, 26, 26, 85]), from yolo-tiny outputs
+    # targets： txy, twh, tcls, indices, from building_traget()
     FT = torch.cuda.FloatTensor if p[0].is_cuda else torch.Tensor
     lxy, lwh, lcls, lconf = FT([0]), FT([0]), FT([0]), FT([0])
     txy, twh, tcls, indices = targets
     MSE = nn.MSELoss()
-    CE = nn.CrossEntropyLoss()
+    CE = nn.CrossEntropyLoss() # yolov3 binary CE
     BCE = nn.BCEWithLogitsLoss()
 
     # Compute losses
     # gp = [x.numel() for x in tconf]  # grid points
     for i, pi0 in enumerate(p):  # layer i predictions, i
         b, a, gj, gi = indices[i]  # image, anchor, gridx, gridy
-        tconf = torch.zeros_like(pi0[..., 0])  # conf
+        # b tensor([0, 0, 0, 0, 0, 0, 0, 0])
+        # a tensor([2, 1, 1, 2, 0, 0, 0, 0])
+        # gj tensor([9, 5, 10, 6, 3, 3,4, 3])
+        # gi tensor([6, 3, 4, 8, 4, 2, 3, 4])
+        tconf = torch.zeros_like(pi0[..., 0])  # conf torch.Size([1, 3, 13, 13]),
         nt = len(b)  # number of targets
 
         # Compute losses
         k = 1  # nt / bs
         if nt:
-            pi = pi0[b, a, gj, gi]  # predictions closest to anchors
+            pi = pi0[b, a, gj, gi]  # predictions closest to anchors, torch.Size([8, 85]), 
             tconf[b, a, gj, gi] = 1  # conf
 
             lxy += (k * 8) * MSE(torch.sigmoid(pi[..., 0:2]), txy[i])  # xy loss
